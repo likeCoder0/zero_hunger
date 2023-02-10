cf879652e0b2c62ef38c2f9874e1dbbb89f1c40a
likeCode0 <2021.anchal.sharma@ves.ac.in>
2023-02-10 08:45:44 +0530

    done

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// import { getDatabase } from "firebase/database";
+// import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DO_AMIN,
@@ -11,5 +14,11 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+// export const database=getDatabase(app);
 export const auth = getAuth(app);
 export default app;
+// firebase.initializeApp(firebaseConfig);
+// var database = firebase.database();
+//   export const auth=firebase.a();
+// export default database;
+  
--- src/pages/address.js
+++ src/pages/address.js
@@ -1,8 +1,62 @@
 import React from "react";
+import { useState } from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
+import {database} from "../firebase";
+
 const Address = () => {
+      
+  const [userData, setUserData] = useState({
+    address: "",
+    city: "",
+    state: "",
+    pin: "",
+  });
+
+  let name, value;
+  const postUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUserData({ ...userData, [name]: value });
+  };
+
+  // connect with firebase
+  const submitData = async (event) => {
+    event.preventDefault();
+    const { address,city,state,pin} = userData;
+
+    if (city && state && pin && address) {
+      const res = fetch(
+        process.env.REACT_APP_DATABASE,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            address,city,state,pin
+          }),
+        }
+      );
+
+      if (res) {
+        setUserData({
+          address: "",
+    city: "",
+    state: "",
+    pin: "",
+        });
+        alert("Data Stored");
+      } else {
+        alert("plz fill the data");
+      }
+    } else {
+      alert("plz fill the data");
+    }
+  };
+
   return (
     <div>
       <Navbar></Navbar>
@@ -19,25 +73,34 @@ const Address = () => {
           <div class="right-side">
             <div class="topic-text">Address details</div>
 
-            <form action="#">
+            <form action="#" method="POST">
               <div class="input-box">
-                <input type="text" placeholder="Address" />
+                <input type="text" placeholder="Address" name="address"
+                          id="" value={userData.address}
+                          onChange={postUserData}/>
               </div>
 
               <div class="input-box">
-                <input type="text" placeholder="City" />
+                <input type="text" placeholder="City" name="city"
+                          id="" value={userData.city}
+                          onChange={postUserData}/>
               </div>
 
               <div class="input-box">
-                <input type="text" placeholder="State" />
+                <input type="text" placeholder="State" name="state"
+                          id="" value={userData.state}
+                          onChange={postUserData}/>
               </div>
 
               <div class="input-box">
-                <input type="text" placeholder="Pin" />
+                <input type="text" placeholder="Pin" name="pin"
+                          id="" value={userData.pin}
+                          onChange={postUserData}/>
               </div>
 
               <div class="contact-button">
-                <input type="button" value="Add Address" />
+                <input type="button" value="Add Address" onClick={submitData} />
+                {/* <button ty onClick={submitData}>Add Address</button> */}
               </div>
             </form>
           </div>
